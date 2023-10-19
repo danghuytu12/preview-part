453f443ffd6b6bd479b5535eb61e337a13f844e1
huytu20 <tudh@sapp.edu.com>
2023-10-19 18:39:24 +0700

    Updates

---
+++ precache-manifest.032415bfdc7d8188dd3af9bc76fe64d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d30c941b89c02293696a797597c28ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71869449f71352f3fd11",
+    "url": "./static/css/main.b9abe225.chunk.css"
+  },
+  {
+    "revision": "d7b1db2d57e862783aad",
+    "url": "./static/js/2.0388e864.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0388e864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71869449f71352f3fd11",
+    "url": "./static/js/main.92c416ce.chunk.js"
+  },
+  {
+    "revision": "d5b5005a613b448d24c7",
+    "url": "./static/js/runtime-main.81b80e32.js"
+  }
+]);
